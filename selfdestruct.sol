1bb096eb6ef5092841825e8e3b3e60957b3399b1
P. Misirov <47452703+misirov@users.noreply.github.com>
2021-11-11 00:02:37 +0100

    Update selfdestruct.sol

--- selfdestruct.sol
+++ selfdestruct.sol
@@ -1,17 +1,31 @@
 // SPDX-License-Identifier: MIT
+//https://betterprogramming.pub/solidity-what-happens-with-selfdestruct-f337fcaa58a7
+
 pragma solidity ^0.8.4;
 
 
 // this contract self destructs when tick() function is called 10 times
+pragma solidity ^0.8.4;
+
 contract Contract {
     
-    uint counter = 0;
-
-    function tick()external {
-        if(counter == 9){
+    // when the contract self destructs, the counter will remain at 0
+    uint public counter = 0;
+    
+    
+    function tick() external returns (uint){
+        if(counter == 10){
             selfdestruct(payable(msg.sender));
         }
+        
         counter += 1;
+        return counter;
     }
-
+    
+    
+    // When the contract self destructs, the new address becomes 0x0000...
+    function contractAddress() public view returns(address){
+        return address(this);
+    }
+    
 }
