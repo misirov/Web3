6b2d47ee4409d0c77a4961f937b371018227cc2e
P. Misirov <47452703+misirov@users.noreply.github.com>
2021-11-10 17:18:31 +0100

    Create simple_escrow.sol

---
+++ simple_escrow.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.5;
+
+contract Escrow {
+    address public depositor;
+    address public beneficiary;
+    address public arbiter;
+
+    constructor(address _arbiter, address _beneficiary ) public payable {
+        arbiter = _arbiter;
+        beneficiary = _beneficiary;
+        depositor = msg.sender;
+    }
+
+    function approve() external {
+        require(msg.sender == arbiter, "Only the arbiter can execute this function");
+        payable(beneficiary).transfer(address(this).balance);
+        
+    }
+}
